040488c97144af46891070df35b63021fae99c6a
Philippe Lemaire <philippe.lemaire@protonmail.com>
2023-08-29 14:18:25 +0200

    created the app trombinoscope

--- alumnihida/settings.py
+++ alumnihida/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "crispy_forms",
     "crispy_bootstrap4",
+    # local apps
+    "trombinoscope",
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "staticfiles"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ trombinoscope/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
+
+app_name = "trombinoscope"
+
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
