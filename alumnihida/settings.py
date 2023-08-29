986b5c5a7812faf4db11cd67573d652afb4cce15
Philippe Lemaire <philippe.lemaire@protonmail.com>
2023-08-29 14:23:20 +0200

    customuser model and first migrations

--- alumnihida/settings.py
+++ alumnihida/settings.py
@@ -104,6 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# custom user model for auth
+AUTH_USER_MODEL = "trombinoscope.CustomUser"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
