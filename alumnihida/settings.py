b42dce8915b09f1c6e36eca80af9a5c446d29a5e
Philippe Lemaire <philippe.lemaire@protonmail.com>
2023-09-24 09:59:36 +0200

    added login_url setting for login_required decorator

--- alumnihida/settings.py
+++ alumnihida/settings.py
@@ -147,3 +147,4 @@ EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
+LOGIN_URL = "authentification/identification"
