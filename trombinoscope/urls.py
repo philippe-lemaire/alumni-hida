7a08bac48d4fdca68e2df30bdc72af6e9d9ac931
Philippe Lemaire <philippe.lemaire@protonmail.com>
2023-09-24 10:35:31 +0200

    upload of pictures finally works, the form needed a special tag, and the form constructor needed request.FILES

--- trombinoscope/urls.py
+++ trombinoscope/urls.py
@@ -9,5 +9,6 @@ app_name = "trombinoscope"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("invite_users", views.invite_users_view, name="invite_users"),
+    path("alumni", views.alumni_list_view, name="alumni_list"),
     path("update_profile/<id>/", views.update_profile_view, name="update_profile"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
