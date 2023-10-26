0c5acbafc76e6970c7eb9ac599e6d8b568ffee74
Philippe Lemaire <philippe.lemaire@protonmail.com>
2023-10-26 11:51:57 +0200

    more in line with what's in manage.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,8 @@
 import os
-from dotenv import load_dotenv
+import dotenv
 
 env_file = ".env"
 
 env = os.path.join(os.getcwd(), env_file)
 if os.path.exists(env):
-    load_dotenv(env)
+    dotenv.read_dotenv(override=True)
